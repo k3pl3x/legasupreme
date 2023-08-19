2a468cad85fb320c9cdc29d479a54d6f24665fb8
k3pl3x <renyauc2001@gmail.com>
2023-08-19 17:29:02 +0200

    versione 3

---
+++ script.js
@@ -0,0 +1,120 @@
+        const checkbox1 = document.getElementById('check1');
+        const table1 = document.getElementById('squadraTable1');
+        const checkbox2 = document.getElementById('check2');
+        const table2 = document.getElementById('squadraTable2');
+        const checkbox3 = document.getElementById('check3');
+        const table3 = document.getElementById('squadraTable3');
+        const checkbox4 = document.getElementById('check4');
+        const table4 = document.getElementById('squadraTable4');
+        const checkbox5 = document.getElementById('check5');
+        const table5 = document.getElementById('squadraTable5');
+        const checkbox6 = document.getElementById('check6');
+        const table6 = document.getElementById('squadraTable6');
+        const checkbox7 = document.getElementById('check7');
+        const table7 = document.getElementById('squadraTable7');
+        const checkbox8 = document.getElementById('check8');
+        const table8 = document.getElementById('squadraTable8');
+        const checkbox9 = document.getElementById('check9');
+        const table9 = document.getElementById('squadraTable9');
+        const checkbox10 = document.getElementById('check10');
+        const table10 = document.getElementById('squadraTable10');
+
+        checkbox1.addEventListener('change', function() {
+            if (checkbox1.checked) {
+                table1.style.order = "1"
+                table1.style.display = "none"
+            } else {
+                table1.style.order = "-1"
+                table1.style.display = "table"
+            }
+        });
+    
+        checkbox2.addEventListener('change', function() {
+            if (checkbox2.checked) {
+                table2.style.order = "1"
+                table2.style.display = "none"
+            } else {
+                table2.style.order = "-1"
+                table2.style.display = "table"
+            }
+        });
+
+        checkbox3.addEventListener('change', function() {
+            if (checkbox3.checked) {
+                table3.style.order = "1"
+                table3.style.display = "none"
+            } else {
+                table3.style.order = "-1"
+                table3.style.display = "table"
+            }
+        });
+
+        checkbox4.addEventListener('change', function() {
+            if (checkbox4.checked) {
+                table4.style.order = "1"
+                table4.style.display = "none"
+            } else {
+                table4.style.order = "-1"
+                table4.style.display = "table"
+            }
+        });
+
+        checkbox5.addEventListener('change', function() {
+            if (checkbox5.checked) {
+                table5.style.order = "1"
+                table5.style.display = "none"
+            } else {
+                table5.style.order = "-1"
+                table5.style.display = "table"
+            }
+        });
+
+        checkbox6.addEventListener('change', function() {
+            if (checkbox6.checked) {
+                table6.style.order = "1"
+                table6.style.display = "none"
+            } else {
+                table6.style.order = "-1"
+                table6.style.display = "table"
+            }
+        });
+
+        checkbox7.addEventListener('change', function() {
+            if (checkbox7.checked) {
+                table7.style.order = "1"
+                table7.style.display = "none"
+            } else {
+                table7.style.order = "-1"
+                table7.style.display = "table"
+            }
+        });
+
+        checkbox8.addEventListener('change', function() {
+            if (checkbox8.checked) {
+                table8.style.order = "1"
+                table8.style.display = "none"
+            } else {
+                table8.style.order = "-1"
+                table8.style.display = "table"
+            }
+        });
+
+        checkbox9.addEventListener('change', function() {
+            if (checkbox9.checked) {
+                table9.style.order = "1"
+                table9.style.display = "none"
+            } else {
+                table9.style.order = "-1"
+                table9.style.display = "table"
+            }
+        });
+
+        checkbox10.addEventListener('change', function() {
+            if (checkbox10.checked) {
+                table10.style.order = "1"
+                table10.style.display = "none"
+            } else {
+                table10.style.order = "-1"
+                table10.style.display = "table"
+            }
+        });
